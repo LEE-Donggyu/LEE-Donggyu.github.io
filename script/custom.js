1aa95afcd0688208a103a51f7e3478b4bad4ac60
LEE-Donggyu <ehdrb141@naver.com>
2024-09-25 02:29:01 +0900

    intro link 작업

--- script/custom.js
+++ script/custom.js
@@ -1,3 +1,22 @@
 $('.resume').click(function() {
-  $('html').animate({scrollTop:0}, 100);
+  $('html, body').animate({scrollTop:0}, 300);
+});
+$('.1st').click(function() {
+  $('html, body').animate({scrollTop:0}, 300);
+});
+$('.2nd').click(function() {
+  var offset = $(".projects").offset();
+  $('html, body').animate({scrollTop: offset.top - 55}, 300);
+});
+$('.3rd').click(function() {
+  var offset = $(".experience").offset();
+  $('html, body').animate({scrollTop: offset.top - 55}, 300);
+});
+$('.4th').click(function() {
+  var offset = $(".award").offset();
+  $('html, body').animate({scrollTop: offset.top - 55}, 300);
+});
+$('.5th').click(function() {
+  var offset = $(".education").offset();
+  $('html, body').animate({scrollTop: offset.top - 55}, 300);
 });
